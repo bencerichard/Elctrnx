72a22ed42db97d9f1a8dc6cd1aa6d5a423193885
hachb <bencehach@gmail.com>
2020-04-02 17:52:09 +0300

    Initial commit

---
+++ src/main/java/com/example/elctrnx/ElctrnxApplication.java
@@ -0,0 +1,13 @@
+package com.example.elctrnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElctrnxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElctrnxApplication.class, args);
+	}
+
+}
