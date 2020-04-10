c91ad7b0c74092d5c1e7950a3847e6968f488404
flocad <flocadariusandrei@gmail.com>
2020-04-11 00:11:33 +0300

    Updated login screen and UI

--- elctrnx/src/app/log-in/log-in.component.ts
+++ elctrnx/src/app/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {
+
+  }
 
   ngOnInit(): void {
   }
 
+  log_in(): void {
+    this.router.navigate(['/products'])
+  }
 }
