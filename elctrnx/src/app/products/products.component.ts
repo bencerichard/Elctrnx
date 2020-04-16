9f67503863f78f58a5f12a74341010a1d4802bf0
hachb <bencehach@gmail.com>
2020-04-16 17:37:01 +0300

    Added href on cards and carousel + prod card draft Refactored some test products data to be more realistic

--- elctrnx/src/app/products/products.component.ts
+++ elctrnx/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {Product} from '../Product';
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-products',
@@ -11,28 +12,53 @@ export class ProductsComponent implements OnInit {
 
   products: Product[] = [
     {
-      id: 5,
-      name: "Product1",
-      price: 100,
-      image: "https://static11.edstatic.net/product_images/470x470/resize/02-dell-inspiron-3576-dll-q1-249739-notebook-windows-10-fekete_fl761aiu.jpg?v=1"
+      id: 1,
+      name: "Iphone 11 Pro 256 Gb",
+      price: 5600,
+      image: "assets/img/iphone11.jpg"
     },
     {
-      id: 6,
-      name: "Product2",
-      price: 200,
-      image: "https://static11.edstatic.net/product_images/470x470/resize/02-dell-inspiron-3576-dll-q1-249739-notebook-windows-10-fekete_fl761aiu.jpg?v=1"
+      id: 1,
+      name: "Samsung Galaxy S20 64 Gb",
+      price: 5600,
+      image: "assets/img/samsungS20.jpg"
+    },
+    {
+      id: 1,
+      name: "Dell 11 Pro 1Tb, 256 SSD full da options",
+      price: 5600,
+      image: "assets/img/dellLogo.png"
+    },
+    {
+      id: 1,
+      name: "Iphone 11 Pro 256 Gb",
+      price: 5600,
+      image: "assets/img/iphone11.jpg"
+    },
+    {
+      id: 5,
+      name: "Product1",
+      price: 5600,
+      image: "assets/img/iphone11.jpg"
+    },
+    {
+      id: 1,
+      name: "Iphone 11 Pro 256 Gb",
+      price: 5600,
+      image: "assets/img/iphone11.jpg"
     }
   ];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private location: Location
   ) {
   }
 
   ngOnInit(): void {
   }
 
-  my_account(): void {
+  searchApple(): void {
     this.router.navigate(['/my-account']);
   }
 
