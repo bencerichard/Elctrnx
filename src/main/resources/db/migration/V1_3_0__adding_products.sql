8cd62cd861d025e45c7f5faf4fb022237c119dd9
hachb <bencehach@gmail.com>
2020-04-20 03:06:07 +0300

    Added Flyway migration

---
+++ src/main/resources/db/migration/V1_3_0__adding_products.sql
@@ -0,0 +1,11 @@
+
+insert into product (product_id, name, product_category_product_category_id, image, price, description)
+values (1, 'Iphone 11 Pro 256 Gb', 2,
+        'https://s12emagst.akamaized.net/products/25344/25343968/images/res_ba5ab105acb7b08cf67af51f5868a942_450x450_qdq2.jpg', 6800,
+        'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedent­ed leap in battery life. And a mind‑blowing chip that elevates machine learning and pushes
+        the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro.');
+insert into product (product_id, name, product_category_product_category_id, image, price, description)
+values (2, 'Samsung Galaxy S20 64 Gb', 2,
+        'https://s12emagst.akamaized.net/products/28451/28450673/images/res_e8507ba30388ea5053e015325ef2a793_450x450_rif2.jpg', 4000,
+        'Meet Galaxy S20. With revolutionary 8K Video Snap changing how you capture not just video but photography. Add in Samsung Knox security,
+         an intelligent battery, powerful processor and massive storage — and the Galaxy S20 unveils a whole new world for mobile.');
---
+++ src/main/resources/db/migration/V1_4_0__adding_locations_addresses.sql
@@ -0,0 +1,5 @@
+insert into address_ (id, city, country, street) values (1, 'Timisoara', 'Romania', 'Calea Aradului 18');
+insert into address_ (id, city, country, street) values (2, 'Bucuresti', 'Romania', 'Șoseaua Virtuții 148');
+
+insert into location (id, name, address_id) values (1, 'Elctrnx Timisoara', 1);
+insert into location (id, name, address_id) values (2, 'Elctrnx Bucuresti', 2);
