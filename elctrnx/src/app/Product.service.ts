1c11faeda0c46a66a357089eb14c7d3ebb05f13d
flocad <flocadariusandrei@gmail.com>
2020-04-20 17:55:44 +0300

    Implemented product CRUD functionality

--- elctrnx/src/app/Product.service.ts
+++ elctrnx/src/app/Product.service.ts
@@ -6,16 +6,34 @@ import {Product} from "./Product";
 @Injectable({providedIn: 'root'})
 export class ProductService {
 
-  private produtcsUrl='http://localhost:8080/products';
+  private productsUrl = 'http://localhost:8080/products/';
 
-  httpOptions={
-    headers:new HttpHeaders({'Content-Type':'application/json'})
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
-  constructor(private http:HttpClient){}
-
-  getProducts():Observable<Product[]>{
-    return this.http.get<Product[]>(this.produtcsUrl);
+  constructor(private http: HttpClient) {
   }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl);
+  }
+
+  getSingleProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(this.productsUrl + id);
+  }
+
+  newProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.productsUrl, product, this.httpOptions);
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+    return this.http.put<Product>(this.productsUrl + id, product, this.httpOptions);
+  }
+
+  deleteProduct(id: number): Observable<Product> {
+    return this.http.delete<Product>(this.productsUrl + id);
+  }
+
 
 }
--- elctrnx/src/app/products/products.component.ts
+++ elctrnx/src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from '../Product';
+import {ProductService} from "../Product.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-products',
@@ -8,12 +10,47 @@ import {Product} from '../Product';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[] =[];
+  products: Product[] = [];
+  product: Product;
 
-  constructor() {
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+  }
+
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
+      }
+    );
+  }
+
+  getSingleProduct(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getSingleProduct(id).subscribe(
+      product => {
+        this.product = product;
+      }
+    )
+  }
+
+  newProduct(categoryName: string, description: string, productName: string, price: number, image: string): void {
+    this.productService.newProduct({categoryName,description,productName,price,image} as Product).subscribe();
+  }
+
+  updateProduct(categoryName: string, description: string, productName: string, price: number, image: string):void{
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.updateProduct(id,{categoryName,description,productName,price,image} as Product).subscribe();
+  }
+
+  deleteProduct():void{
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.deleteProduct(id).subscribe();
   }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
 
