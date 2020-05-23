298b705de1873e777b78970a96d8081c282130c1
hachb <bencehach@gmail.com>
2020-05-24 01:49:26 +0300

    please5

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
 	res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+});
+
+// app.get('*', (req, res) => {
+//   res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
 console.log('Console listening');
