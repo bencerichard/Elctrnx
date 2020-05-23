7751a4b8009196a93d7853b79625bdd7dc3a38a4
hachb <bencehach@gmail.com>
2020-05-24 01:06:14 +0300

    please 3

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Product} from './product';
+import {Product} from './Product';
 import {Stock} from './Stock';
 
 @Injectable({providedIn: 'root'})
